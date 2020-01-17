c89eeaaebdf599b55bbe5ddf92ca9d35d4919273
haesooKr <gotn676@gmail.com>
2020-01-17 16:24:25 -0500

    Test useTabs.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import useInput from './hooks/useInput';
+import useTabs from './hooks/useTabs';
 
 function App() {
-  const noA = value => !value.includes('a');
-  const name = useInput("Mr.", noA);
-
+  const contents = [
+    {
+      title: 1,
+      content: "I am tab 1!"
+    },
+    {
+      title: 2,
+      content: "I am tab 2!"
+    },
+    {
+      title: 3,
+      content: "I am tab 3!"
+    },
+    {
+      title: 4,
+      content: "I am tab 4!"
+    },
+  ];
+  const tabs = useTabs(0, contents);
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <input onChange={name.onChange} placeholder="Name" value={name.value}/>
+      {contents.map((tab, i) => <button key={i} onClick={() => tabs.changeTab(i)}>{tab.title}</button>)}
+      {tabs.currentTab.content}
     </div>
   );
 }
