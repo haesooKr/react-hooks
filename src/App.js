167be192c3e5c4d61f6b44451150c2a74979d504
haesooKr <gotn676@gmail.com>
2020-01-17 17:50:56 -0500

    Test useTitle.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,19 @@
 import React from 'react';
-import useTabs from './hooks/useTabs';
+import useTitle from './hooks/useTitle';
+
 
 function App() {
-  const contents = [
-    {
-      title: 1,
-      content: "I am tab 1!"
-    },
-    {
-      title: 2,
-      content: "I am tab 2!"
-    },
-    {
-      title: 3,
-      content: "I am tab 3!"
-    },
-    {
-      title: 4,
-      content: "I am tab 4!"
-    },
-  ];
-  const tabs = useTabs(0, contents);
+  const titleUpdate = useTitle('Loading...');
+  const time = async () => {
+    console.log('timer start')
+    await setTimeout(() => {
+      titleUpdate('Haesoo')
+      console.log('title changed');
+    }, 5000);
+  }
   return (
     <div className="App">
-      {contents.map((tab, i) => <button key={i} onClick={() => tabs.changeTab(i)}>{tab.title}</button>)}
-      {tabs.currentTab.content}
+      <button onClick={() => time()}>Title</button>
     </div>
   );
 }
