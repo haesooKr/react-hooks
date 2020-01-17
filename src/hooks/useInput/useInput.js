abdbbda92980ba027a0782433234ef684fd8b1ed
haesooKr <gotn676@gmail.com>
2020-01-17 16:08:00 -0500

    Added useInput.js

---
+++ src/hooks/useInput/useInput.js
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+export const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (e) => {
+    const value = e.target.value;
+    let willUpdate = true;
+    if (typeof validator === "function"){
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  }
+  return { value, onChange };
+}
