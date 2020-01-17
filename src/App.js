e4abeda80a4d5ee23fc35cc04e7736d9ab31b349
haesooKr <gotn676@gmail.com>
2020-01-17 16:08:13 -0500

    Test useInput.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import useInput from './hooks/useInput';
 
 function App() {
+  const noA = value => !value.includes('a');
+  const name = useInput("Mr.", noA);
+
   return (
     <div className="App">
-
+      <h1>Hello</h1>
+      <input onChange={name.onChange} placeholder="Name" value={name.value}/>
     </div>
   );
 }
